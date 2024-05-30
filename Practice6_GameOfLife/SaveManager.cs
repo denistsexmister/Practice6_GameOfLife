5a8d179e27a1d6fe4a096b00abfb39a9398a92ad
dxrknesss <imp0rt4ntduck@gmail.com>
2024-05-30 10:53:50 +0300

    Refactored load file name logic to SaveManager

--- Practice6_GameOfLife/SaveManager.cs
+++ Practice6_GameOfLife/SaveManager.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Xaml.Controls;
 
 namespace Practice6_GameOfLife
 {
     internal class SaveManager
     {
+        private static string saveFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\gameoflife";
+
+        async public static void SaveFile(string fileName, string savedData)
+        {
+            if (fileName == null || fileName.Equals(""))
+            {
+                fileName = "default" + DateTime.Now + ".txt";
+            }
+
+            if (!Directory.Exists(saveFolderPath))
+            {
+                Directory.CreateDirectory(saveFolderPath);
+            }
+            StorageFolder savesFolder = await StorageFolder.GetFolderFromPathAsync(saveFolderPath);
+            StorageFile saveFile = await savesFolder.CreateFileAsync(fileName, CreationCollisionOption.GenerateUniqueName);
+
+            await FileIO.WriteTextAsync(saveFile, savedData);
+        }
+
+        async public static Task<IReadOnlyList<StorageFile>> ReadFileNamesFromSavedFolder()
+        {
+            if (Directory.Exists(saveFolderPath))
+            {
+                StorageFolder savesFolder = await StorageFolder.GetFolderFromPathAsync(saveFolderPath);
+                IReadOnlyList<StorageFile> files = await savesFolder.GetFilesAsync();
+                return files;
+            }
+            else { return new List<StorageFile>(); }
+        }
     }
 }
